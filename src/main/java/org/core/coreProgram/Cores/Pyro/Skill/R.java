1bf93ad688a5abad3934c943d0a531d29abcebce
BenOrHy <soohyun324@gmail.com>
2025-07-21 15:26:02 +0900

    Pyro interface set

--- src/main/java/org/core/coreProgram/Cores/Pyro/Skill/R.java
+++ src/main/java/org/core/coreProgram/Cores/Pyro/Skill/R.java
@@ -1,4 +1,26 @@
 package org.core.coreProgram.Cores.Pyro.Skill;
 
-public class R {
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.core.Cool.Cool;
+import org.core.coreProgram.Abs.SkillBase;
+import org.core.coreProgram.Cores.Pyro.coreSystem.Pyro;
+
+public class R implements SkillBase {
+
+    private final Pyro config;
+    private final JavaPlugin plugin;
+    private final Cool cool;
+
+    public R(Pyro config, JavaPlugin plugin, Cool cool) {
+        this.config = config;
+        this.plugin = plugin;
+        this.cool = cool;
+    }
+
+    @Override
+    public void Trigger(Player player){
+
+    }
+
 }
