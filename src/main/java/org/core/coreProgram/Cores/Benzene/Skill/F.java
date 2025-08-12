8afcd1a07fc2637f50719244fb68f3d6e7b4f454
BenOrHy <soohyun3224@gmail.com>
2025-08-12 10:47:31 +0900

    Benzene f skill gradation setting

--- src/main/java/org/core/coreProgram/Cores/Benzene/Skill/F.java
+++ src/main/java/org/core/coreProgram/Cores/Benzene/Skill/F.java
@@ -269,9 +269,9 @@ public class F implements SkillBase {
                         }else{
                             Particle.DustOptions dustOptions = new Particle.DustOptions(Color.fromRGB(66, 66, 66), 0.6f);
                             world.spawnParticle(Particle.DUST, particleLocation.add(0, height, 0), 1, 0, 0, 0, 0, dustOptions);
-                            if(Math.random() > 0.11){
+                            if(Math.random() < 0.11){
                                 Particle.DustOptions dustOptions_gra = new Particle.DustOptions(Color.fromRGB(33, 33, 33), 0.6f);
-                                world.spawnParticle(Particle.DUST, particleLocation.add(0, height, 0), 1, 0, 0, 0, 0, dustOptions_gra);
+                                world.spawnParticle(Particle.DUST, particleLocation, 1, 0, 0, 0, 0, dustOptions_gra);
                             }
                         }
 
